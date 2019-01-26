e6d2386f0cc9d061de49827f481deaa10ddf4a9a
Cambrea <42923149+earley24@users.noreply.github.com>
2019-01-26 15:03:34 -0500

    Add files via upload

---
+++ BinaryClass.py
@@ -0,0 +1,8 @@
+#Binary class written  by Cambrea Earley
+#This class creates a binary string and implements &,|,~,^
+#bit operations are supported between Binary class instances
+
+class Binary:
+    def __init__(x1,x2,x3,x4):
+        self.binString = [x1,x2,x3,x4]
+        
